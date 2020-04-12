79d4d5f15e89a27694bb2fb1aa7339f94d1e732a
Kun Hu <kun.hu7602@gmail.com>
2020-04-12 16:45:40 -0700

    udpate comments

--- 2020/1a/q2.py
+++ 2020/1a/q2.py
@@ -3,12 +3,20 @@ rri = lambda: int(rr())
 
 '''
 In Pascal triangle, the r-th (base 1) row sums up to 2^(r-1).
-Given a total N, look at its binary form, from the least
-significant bit:
-1. if the x-th bit is 1, we need to grab all cells from x-th row in the triangle;
+Given N, look at its binary form, from the least significant bit:
+1. if the x-th bit is 1, we need to grab the whole x-th row in the triangle;
 2. otherwise, we need to skip the x-th row.
-But to form a consecutive path, we can't completely skip a row. Instead, we can
+But to form a consecutive path, we can't completely skip a row.
 
+We know that because N <= 10^9, and 10^9 < 2^30, so in total we would
+use less than 30 rows.
+
+To get closer to the answer, we can go down one side of the triangle with 1s,
+if we see 0 at the least significant bits.
+And when we see 1, we take the whole row, and go to the other side of the triangle.
+We will have to take extra 1s when seeing 0 at the bits, so we can first try to
+construct N - 30, and keep going down one side of the triangle afterwards to collect
+the rest using 1s.
 '''
 def solve(n):
     def output(a, b):
